28dc4b06f71d53f0bf161d862d5269cdb3c8c3e1
Stuart Walsh <stu@ipng.org.uk>
2015-07-28 11:19:56 +0100

    Move the API stuff out into separate controllers

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -1,82 +1,5 @@
 'use strict';
 
-var accountRepository = require('../accountrepository.js');
-var channelRepository = require('../channelrepository.js');
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
-var config = require('../config.js');
-
-function accountLogin(request, reply) {
-    accountRepository.getByNick(request.payload.nickname, function(result) {
-        if(!result) {
-            return reply({ error: 'Invalid username or password' });
-        }
-
-        var shasum = crypto.createHash('sha1');
-        shasum.update(request.payload.password + result.salt);
-        var hash = shasum.digest('hex').toUpperCase();
-
-        if(hash !== result.password.toUpperCase()) {
-            return reply({ error: 'Invalid username or password' });
-        }
-
-        var nickname = {
-            id: result.id + '',
-            nickname: result.nick,
-            email: result.email,
-            admin: result.flag_admin
-        };
-
-        nickname.token = jwt.sign(nickname, config.tokenSecret);
-
-        reply(nickname).state('authToken', nickname.token);
-    });
-}
-
-function accountLogout(request, reply) {
-    reply.unstate('authToken');
-    return reply.redirect('/');
-}
-
-function accountGet(request, reply) {
-    var id = request.auth.credentials.id;
-
-    accountRepository.getById(id, function(result) {
-        return reply({
-            cloak: result.cloak,
-            email: result.email,
-            url: result.url,
-            lastHost: result.last_host,
-            lastQuitMessage: result.last_quit_message,
-            lastQuitTime: result.last_quit_time,
-            lastRealname: result.last_realname,
-            enforce: result.flag_enforce,
-            secure: result.flag_secure,
-            cloakEnabled: result.flag_cloak_enabled,
-            private: result.flag_private,
-            regTime: result.reg_time
-        });
-    });
-}
-
-function accountNicknames(request, reply) {
-    accountRepository.getNicknames(request.auth.credentials.id, function(result) {
-        reply(result);
-    });
-}
-
-function accountCertificates(request, reply) {
-    accountRepository.getCertificates(request.auth.credentials.id, function(result) {
-        reply(result);
-    });
-}
-
-function accountChannels(request, reply) {
-    channelRepository.getChannelsForAccount(request.auth.credentials.id, function(result) {
-        reply(result);
-    });
-}
-
 module.exports.init = function(server) {
     server.state('authToken', {
         ttl: null,
@@ -92,44 +15,20 @@ module.exports.init = function(server) {
         method: 'GET',
         path: '/login',
         handler: function(request, reply) {
-            reply.view('login', { authenticated: request.auth.isAuthenticated, activeLogin: true });
+            reply.view('login', {
+                authenticated: request.auth.isAuthenticated,
+                activeLogin: true
+            });
         }
     });
 
     server.route({
         method: 'GET',
         path: '/logout',
-        handler: accountLogout
-    });
-
-    server.route({
-       method: 'POST',
-       path: '/api/login',
-       handler: accountLogin
-    });
-
-    server.route({
-       method: 'GET',
-       path: '/api/account/{id?}',
-       handler: accountGet
-    });
-
-    server.route({
-       method: 'GET',
-       path: '/api/account/nicknames/{id?}',
-       handler: accountNicknames
-    });
-
-    server.route({
-       method: 'GET',
-       path: '/api/account/certificates/{id?}',
-       handler: accountCertificates
-    });
-
-    server.route({
-       method: 'GET',
-       path: '/api/account/channels/{id?}',
-       handler: accountChannels
+        handler: function (request, reply) {
+            reply.unstate('authToken');
+            return reply.redirect('/');
+        }
     });
 
     server.route({
--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,15 +1,9 @@
 'use strict';
 
 var account = require('./account.js');
-var nickname = require('./nickname.js');
 var channel = require('./channel.js');
-
-var api = require('./api');
 
 module.exports.init = function(server) {
     account.init(server);
-    nickname.init(server);
     channel.init(server);
-
-    api.init(server);
 };
