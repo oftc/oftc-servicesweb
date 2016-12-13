2a9e9701cb73eaf5597a53f967f1b3b9a48bcfc1
Stuart Walsh <stu@ipng.org.uk>
2016-12-13 13:21:01 +0000

    Add missing package and redirect to login on any 401 error. Should Fix #1

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,3 +5,9 @@
 $(document).ajaxSend(function(event, jqxhr, settings) {
 	// This will set up the authentication headers for the API
 });
+
+$(document).ajaxError((event, xhr) => {
+	if(xhr.status === 401) {
+		location.href = '/login';
+	}
+});
