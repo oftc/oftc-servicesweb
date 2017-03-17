947d8dad35717de2a75b6f5721ba35237315754c
Christoph Berg <myon@debian.org>
2017-03-17 09:45:33 +0100

    Revert "Add missing package and redirect to login on any 401 error. Should Fix #1"
    This reverts commit 2a9e9701cb73eaf5597a53f967f1b3b9a48bcfc1.

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,9 +5,3 @@
 $(document).ajaxSend(function(event, jqxhr, settings) {
 	// This will set up the authentication headers for the API
 });
-
-$(document).ajaxError((event, xhr) => {
-	if(xhr.status === 401) {
-		location.href = '/login';
-	}
-});
